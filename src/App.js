2e0842ae07828771db1f8c6e7daf51c58304e1ee
musaabdullah <musaabdalwahed@gmail.com>
2021-03-28 23:10:54 +0200

    update data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function App() {
+
+  const [title, setTitle] = useState();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <div className="">
+       <input type="text" className="" value={title} onChange={(e) => setTitle(e.target.value)}  placeholder="Enter Todo" />
+       <button onClick={() =>  dispatch({ type:"ADD_TODO", payload:{title} })}>Save</button>
+     </div>
+     {
+       state.map((item) => {
+         return <div key={item.id}>
+          <div><span className={`${item.completed? "line": ""}`}>{item.title}</span> <button onClick={() => dispatch({ type:"REMOVE_TODO", payload:{ id:item.id }})}>Remove</button>
+          <button onClick={() => setTitle(item.title)}>Select item</button>
+          <button onClick={() => dispatch({ type: "UPDATE_TODO", payload: {id: item.id, title: title}})}>update</button>
+          <button onClick={() => dispatch({ type: "COMPLETED_TODO", payload: {id: item.id, completed: item.completed}})}>completed</button>
+          </div>
+         </div>
+       })
+     }
     </div>
   );
 }
